8559237c07f722b52d8869ba2136df2b3a0385a4
Daniel Bustos Ruiz <daniel.bustos935@hotmail.com>
2023-10-12 15:59:47 -0600

    Se creo la clase de la red neuronal en el archivo "mpl.py" y se inicializan los valores para trabajar con la red neuronal

---
+++ mpl.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+#!Creamos la clase base para la red neuronal que toma como clase padre la clase "nn" de pytorch
+class NeuralNet(nn.Module):
+
+    #*El constructor toma el numero de inputs, capas, neuronas y salias y el nombre del archivo para tomar las entradas y salidas
+    def __init__(self, noInputs, noCapas, noNeuronas, noOutput, file):
+        #*Llama a la clase padre para hacer nuestro modelo de la red neuronal
+        super(NeuralNet, self).__init__()
+
+        #*Crea la primer capa de entrada y de salida
+        self.capaInput = nn.Linear(noInputs, noNeuronas)
+        self.capaOutput = nn.Linear(noNeuronas, noOutput)
+
+        #*Crea las capas ocultas y las almacena en una lista de torch
+        #*Cada capa del tipo lineal
+        self.capasOcultas = nn.ModuleList()
+        for i in range(noCapas):
+            self.capasOcultas.append(nn.Linear(noNeuronas, noNeuronas))
+
+    #*Metodo para hacer el feedforward
+    def forward(self, inputs):
+        #*Comienza por recorrer todas las capas ocultas y usar la funcion de activacion con el fin de obtener una prediccion
+        #*Inicia con la capa de entrada y recorre la lista creada de torch, se va a usar la funcion sigmoide
+        prediction = torch.sigmoid(self.capaInput(inputs))
+        for capa in self.capasOcultas:
+            prediction = torch.sigmoid(capa(prediction))
+        #*Obtiene la prediccion final (capa de salida)
+        prediction = torch.sigmoid(self.capaOutput(prediction))
+        return prediction
--- practica_4.py
+++ practica_4.py
@@ -1,3 +1,49 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+#!Importamos la clase de la red neuronal
+from mpl import NeuralNet
+    
+#!Funcion para leer el archivo csv
+def readFile(f):
+
+    data = pd.read_csv(f)
+
+    #*El archivo tiene cuatro columnas como entradas y tres de salidas
+    inputs = data.iloc[:,:4]
+    outputs = data.iloc[:,4:]
+
+    #*Usando la libreria de sklearn vamos a dividir el set de datos en 80% para train y 20% para test
+    inputTrain, inputTest, outputTrain, outputTest = train_test_split(inputs, outputs, test_size=0.2, random_state=2)
+
+    print(f"{inputTrain.shape}, {inputTest.shape}, {outputTrain.shape}, {outputTest.shape}")
+
+    #*Regresa todos los sets creados
+    return inputs, outputs, inputTrain, inputTest, outputTrain, outputTest
+
+#!Creamos las variables para guardar inputs y los tensores
+#*Leemos el archivo
+inputs, outputs, inputTrain, inputTest, outputTrain, outputTest = readFile("irisbin.csv")
+
+print(inputTest)
+
+#*Variables para guardar los tensores de la red neuronal
+#*Los tensores se van a configurar para que los ejecute el cpu
+tenInputTrain = torch.from_numpy(inputTrain.values).float().to("cpu")
+tenInputTest = torch.from_numpy(inputTest.values).float().to("cpu")
+tenOutputTrain = torch.from_numpy(outputTrain.values).int().to("cpu")
+tenOutputTest = torch.from_numpy(outputTest.values).int().to("cpu")
+
+#!Funcion de perdida diseñada para este problema en especifico
+def loss(predic, esperado):
+    #*Usamos el error cuadratico medio
+    return torch.mean((predic - esperado)**2)
+
+#!Creamos las variables que seran utilizadas para la red neuronal
+learningRate = 0.001
+epochs = 1000
+
